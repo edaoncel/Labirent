7ae7c7d4c0a29e9f1a6e52ae218264b2951bc703
eda oncel <edaoncel@email.com>
2023-05-19 14:36:46 +0300

    Labirent Projesi

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labirent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button4_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button5_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button6_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button7_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button8_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button9_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+
+        private void button10_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("kaldınız");
+        }
+    }
+}
